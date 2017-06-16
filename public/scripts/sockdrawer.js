a9d6aa565e1c177bca2585d421b4675fa74fadc8
Peter Wildberger <pete.wildberger@gmail.com>
2017-06-16 10:28:44 -0500

    add ng-init

--- public/scripts/sockdrawer.js
+++ public/scripts/sockdrawer.js
@@ -4,5 +4,9 @@ app.controller('SocksController', SocksController);
 
 function SocksController(){
 console.log('NG YO');
+var vm = this;
+vm.getSocks =function(){
+  console.log('get socks');
+};
 
 }
